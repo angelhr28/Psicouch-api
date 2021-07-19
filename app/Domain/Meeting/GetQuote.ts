89bc4047ab387a87ac581f3ab5648f5cfe841172
angelhr28 <newton.huamannahui@promart.pe>
2021-07-18 22:46:17 -0500

    aceptar secretaria

--- app/Domain/Meeting/GetQuote.ts
+++ app/Domain/Meeting/GetQuote.ts
@@ -6,8 +6,8 @@ export default class GetQuote implements QueryObject {
     
     public query(): string {
         return `
-            select m.id                                             as meeting_id,-
-                concat('Paquete: ', p.name, ' - Tema: ', m.name) as name
+            select m.id as meeting_id,
+                   concat('Paquete: ', p.name, ' - Tema: ', m.name) as name
             from meetings as m
                      inner join quotes q on m.id = q.meeting_id
                      inner join products p on m.product_id = p.id
